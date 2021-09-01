7dd5353c631ddad7a4bc608a1792d8061d73fa2a
Constantin A <git-user@c1710.de>
2021-09-01 15:50:08 +0200

    FontRequestEmojiPack gets additional parameters

--- filemojicompat-ui/src/main/java/de/c1710/filemojicompat_ui/packs/FontRequestEmojiPack.kt
+++ filemojicompat-ui/src/main/java/de/c1710/filemojicompat_ui/packs/FontRequestEmojiPack.kt
@@ -6,32 +6,54 @@ import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.content.pm.ProviderInfo
 import android.graphics.drawable.Drawable
+import android.net.Uri
 import androidx.core.provider.FontRequest
 import androidx.emoji2.text.EmojiCompat
 import androidx.emoji2.text.FontRequestEmojiCompatConfig
 import de.c1710.filemojicompat_ui.helpers.EmojiPackList
 import de.c1710.filemojicompat_ui.structures.EmojiPack
+import de.c1710.filemojicompat_ui.structures.Version
 
 // TODO: This might pose a security issue due to missing certificate/signature checks...
 class FontRequestEmojiPack(
-    private val provider: ProviderInfo,
-    packageManager: PackageManager,
+    var request: FontRequest,
+    id: String,
+    name: String,
+    description: String = "TODO",
+    private val icon: Drawable?,
+    version: Version? = null,
+    website: Uri? = null,
+    license: Uri? = null,
+    descriptionLong: String = "TODO looooooong"
 ) : EmojiPack(
-    provider.packageName.replace('.', '_').replace('-', '_') + "_" + provider.name,
-    provider.name,
-    "TODO",
-    null,
-    null,
-    null,
-    "TODO looooooong"
+    id,
+    name,
+    description,
+    version,
+    website,
+    license,
+    descriptionLong
 ) {
-    val icon = provider.loadIcon(packageManager)
+
+    constructor(
+        provider: ProviderInfo,
+        packageManager: PackageManager,
+        id: String = provider.packageName.replace('.', '_').replace('-', '_') + "_" + provider.name,
+        name: String = provider.name,
+        description: String = "TODO",
+        icon: Drawable? = provider.loadIcon(packageManager),
+        version: Version? = null,
+        website: Uri? = null,
+        license: Uri? = null,
+        descriptionLong: String = "TODO looooooong"
+    ): this(
+        FontRequest(provider.authority, provider.packageName, DEFAULT_EMOJI_QUERY, ArrayList()),
+        id, name, description, icon, version, website, license, descriptionLong
+    )
 
     override fun load(context: Context, list: EmojiPackList): EmojiCompat.Config {
         // TODO: Signatures?!
-        val fontRequest =
-            FontRequest(provider.authority, provider.packageName, DEFAULT_EMOJI_QUERY, ArrayList())
-        return FontRequestEmojiCompatConfig(context, fontRequest)
+        return FontRequestEmojiCompatConfig(context, request).setReplaceAll(true)
     }
 
     override fun isCurrentVersion(list: EmojiPackList): Boolean = true
